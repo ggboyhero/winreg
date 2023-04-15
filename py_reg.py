48b0de36394f562008a2b701842542f24ca85d75
ggboyhero <130449163+ggboyhero@users.noreply.github.com>
2023-04-14 23:59:57 -0800

    Create py_reg.py

---
+++ py_reg.py
@@ -0,0 +1,18 @@
+# importing required module
+import winreg as wrg
+  
+# Store location of HKEY_CURRENT_USER
+location = wrg.HKEY_CURRENT_USER
+  
+# Store path in soft
+soft = wrg.OpenKeyEx(location, r"SOFTWARE\\")
+key_1 = wrg.CreateKey(soft, "Geeks")
+  
+# Creating values in Geeks
+wrg.SetValueEx(key_1, "value One", 0, wrg.REG_SZ,
+               "GeeksforGeeks is Best")
+wrg.SetValueEx(key_1, "value Two", 0, wrg.REG_SZ,
+               "Participate in Technical Scripter")
+  
+if key_1:
+    wrg.CloseKey(key_1)
